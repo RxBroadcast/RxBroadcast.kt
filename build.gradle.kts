1fecf931b32e3e68485b4cb7515492e360f9ec8b
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2017-07-10 22:52:59 -0230

    Add empty Gradle build file

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+repositories {
+    jcenter()
+}
+
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.1.3"
+}
+
+dependencies {
+    compile("org.jetbrains.kotlin:kotlin-stdlib-jre8")
+    compile("io.reactivex:rxjava:1.3.0")
+    compile("rxbroadcast:rxbroadcast:1.2.0")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
